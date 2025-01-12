f371119258752d88a085e304c745117ad5cae02b
王喻杰 <11706987+asdnasduanda@user.noreply.gitee.com>
2025-01-12 13:07:41 +0800

    Lua脚本-线程安全性问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,10 +1,9 @@
 package com.hmdp.utils;
-
 import cn.hutool.core.util.BooleanUtil;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
-
-import javax.annotation.Resource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +11,12 @@ public class SimpleRedisLock implements Ilock
 {
     private String name;
     private StringRedisTemplate redisTemplate;
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate redisTemplate) {
         this.name = name;
@@ -28,16 +33,24 @@ public class SimpleRedisLock implements Ilock
         Boolean success = redisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, ThreadId, timeoutSec, TimeUnit.SECONDS);
         return BooleanUtil.isTrue(success);
     }
-
+    //基于lua脚本执行
     @Override
     public void unlock() {
-        //获取线程标识
-        String ThreadId = ID_PREFIX + Thread.currentThread().getId();
-        //判断标识是否一致
-        String id = redisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if(ThreadId.equals(id))
-        {
-            redisTemplate.delete(KEY_PREFIX + name);
-        }
+        //调用lua脚本
+        redisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+                );
     }
+//    @Override
+//    public void unlock() {
+//        //获取线程标识
+//        String ThreadId = ID_PREFIX + Thread.currentThread().getId();
+//        //判断标识是否一致
+//        String id = redisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if(ThreadId.equals(id))
+//        {
+//            redisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
