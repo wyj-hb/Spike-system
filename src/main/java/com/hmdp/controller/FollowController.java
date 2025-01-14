06451e145bacc5ad7b2a05be5a487411ae67f0db
王喻杰 <11706987+asdnasduanda@user.noreply.gitee.com>
2025-01-14 18:26:02 +0800

    共同关注

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,12 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
-
+    @Resource
+    private IFollowService iFollowService;
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long id,@PathVariable("isFollow") Boolean isFollow)
+    {
+        return iFollowService.follow(id,isFollow);
+    }
+    @PutMapping("/or/not/{id}")
+    public Result followOrNot(@PathVariable("id") Long id)
+    {
+        return iFollowService.followOrNot(id);
+    }
+    @GetMapping("/common/{id}")
+    public Result followCommons(@PathVariable("id") Long id)
+    {
+        return iFollowService.followCommons(id);
+    }
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -16,5 +30,65 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IUserService iUserService;
+    @Override
+    public Result follow(Long id, Boolean isFollow) {
+        Long UserId = UserHolder.getUser().getId();
+        //把用户的关注加入到关注列表中
+        String key = "follows:" + UserId;
+        //判断关注还是取关
+        if(isFollow)
+        {
+            //关注则新增数据
+            Follow follow = new Follow();
+            follow.setUserId(UserId);
+            follow.setFollowUserId(id);
+            boolean save = save(follow);
+            if(save)
+            {
 
+                stringRedisTemplate.opsForSet().add(key,id.toString());
+            }
+        }
+        //取关则删除
+        else
+        {
+            boolean sucess = remove(new QueryWrapper<Follow>().eq("user_id", UserId).eq("follow_user_id", id));
+            //移除
+            if(sucess)
+            {
+                stringRedisTemplate.opsForSet().remove(key,id.toString());
+            }
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result followOrNot(Long id) {
+        Long UserId = UserHolder.getUser().getId();
+        //查询是否关注
+        Integer count = query().eq("user_id", UserId).eq("follow_user_id", id).count();
+        return Result.ok(count > 0);
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+        //获取当前用户
+        Long UserId = UserHolder.getUser().getId();
+        String key1 = "follows:" + UserId;
+        //求取交集
+        String key2 = "follows:" + id;
+        Set<String> res = stringRedisTemplate.opsForSet().intersect(key1, key2);
+        if(res == null || res.isEmpty())
+        {
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = res.stream().map(Long::valueOf).collect(Collectors.toList());
+        List<UserDTO> users = iUserService.listByIds(ids).stream().map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(users);
+    }
 }
