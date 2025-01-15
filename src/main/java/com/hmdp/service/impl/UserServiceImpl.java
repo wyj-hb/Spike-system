7834e3f46257d049963f72f2bda570976673a31f
王喻杰 <11706987+asdnasduanda@user.noreply.gitee.com>
2025-01-15 18:26:54 +0800

    签到

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -13,16 +13,22 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -100,5 +106,64 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //保存
         save(build);
         return build;
+    }
+
+    @Override
+    public Result sign() {
+        //1.获取当前登录用户
+        Long id = UserHolder.getUser().getId();
+        //2.获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //3.拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + id + keySuffix;
+        //4.获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+        //5.写入Redis
+        stringRedisTemplate.opsForValue().setBit(key,dayOfMonth - 1,true);
+        return Result.ok();
+    }
+
+    @Override
+    public Result staticSign() {
+        //获取截止到今天的所有签到记录
+        //1.获取当前登录用户
+        Long id = UserHolder.getUser().getId();
+        //2.获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //3.拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + id + keySuffix;
+        //4.获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+        //5.获取本月截止今天为止的所有的签到记录，返回的是一个十进制的数字
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key, BitFieldSubCommands.create().get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth))
+                        .valueAt(0)
+        );
+        if(result == null || result.isEmpty())
+        {
+            return Result.ok(0);
+        }
+        Long num = result.get(0);
+        int cnt = 0;
+        //6.循环遍历
+        while(true)
+        {
+            //7.让这个数字与1做与运算，得到数字的最后一个bit位
+            if((num & 1) == 0)
+            {
+                //为0则签到结束
+                break;
+            }
+            else
+            {
+                //为1则说明已签到，计数器+1
+                cnt++;
+                //把数字右移一位，抛弃最后一个bit位，继续下一个bit位
+                num >>>=1;
+            }
+        }
+        return Result.ok(cnt);
     }
 }
